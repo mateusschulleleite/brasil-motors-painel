2d06efd9b6d7eece4fa900adcc1b81896b8ec08b
Mateus Schulle Leite <mateusschulleleite@gmail.com>
2025-02-23 14:41:21 -0300

    Close modal pronto e menu navegação mobile realizdo

--- src/components/CloseModal/CloseModal.jsx
+++ src/components/CloseModal/CloseModal.jsx
@@ -1,7 +1,12 @@
 import './CloseModal.scss';
+import PropTypes from "prop-types";
 
 export default function CloseModal(props) {
   return (
     <div style={{ display: props.display }} className="close__modal"></div>
   );
+}
+
+CloseModal.propTypes = {
+    display: PropTypes.string.isRequired
 }
