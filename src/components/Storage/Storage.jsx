1732f17db25e0c146b1f613dfcdef8905b7e80d3
Mateus Schulle Leite <mateusschulleleite@gmail.com>
2025-02-23 16:39:14 -0300

    Listagem de carros pronta

--- src/components/Storage/Storage.jsx
+++ src/components/Storage/Storage.jsx
@@ -1,97 +1,122 @@
-import AddEditCar from "../AddEditCar/AddEditCar";
 import "./Storage.scss";
-import PropTypes from "prop-types";
 
-export default function Storage({ storage }) {
-  
-  function addEditCar() {
-  }
-
+export default function Storage() {
   return (
     <div className="storage">
       <h2>Estoque</h2>
-      <div className="storage__buttons">
+      <div className="storage__list">
         <ul>
-        <li>
-            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#C5291C">
-              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-              <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
-              <g id="SVGRepo_iconCarrier">
-                <g id="Complete">
-                  <g data-name="add" id="add-2">
-                    <g>
-                      <line fill="none" stroke="#C5291C" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="12" x2="12" y1="19" y2="5"></line>
-                      <line fill="none" stroke="#C5291C" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="5" x2="19" y1="12" y2="12"></line>
-                    </g>
-                  </g>
-                </g>
-              </g>
-            </svg>
-            <span>Alterar</span>
+          <li>
+            <div className="storage__list-image">
+              <img src="https://quatrorodas.abril.com.br/wp-content/uploads/2024/02/FiatMobiLikeMY245.jpg?quality=70&strip=info" />
+            </div>
+            <div className="storage__list-details">
+              <h3>Fiat Mobi Like 1.0</h3>
+              <p>Ano mod: 2011</p>
+              <p>Ano Fab: 2010</p>
+              <p>Cor: Vermelho</p>
+              <p className="storage__list-details-preco">R$ 50.000</p>
+            </div>
           </li>
           <li>
-            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#C5291C">
-              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-              <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
-              <g id="SVGRepo_iconCarrier">
-                <g id="Complete">
-                  <g data-name="add" id="add-2">
-                    <g>
-                      <line fill="none" stroke="#C5291C" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="12" x2="12" y1="19" y2="5"></line>
-                      <line fill="none" stroke="#C5291C" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="5" x2="19" y1="12" y2="12"></line>
-                    </g>
-                  </g>
-                </g>
-              </g>
-            </svg>
-            <span>Adicionar</span>
+            <div className="storage__list-image">
+              <img src="https://quatrorodas.abril.com.br/wp-content/uploads/2024/02/FiatMobiLikeMY245.jpg?quality=70&strip=info" />
+            </div>
+            <div className="storage__list-details">
+              <h3>Fiat Mobi Like 1.0</h3>
+              <p>Ano mod: 2011</p>
+              <p>Ano Fab: 2010</p>
+              <p>Cor: Vermelho</p>
+              <p className="storage__list-details-preco">R$ 50.000</p>
+            </div>
           </li>
+          <li>
+            <div className="storage__list-image">
+              <img src="https://quatrorodas.abril.com.br/wp-content/uploads/2024/02/FiatMobiLikeMY245.jpg?quality=70&strip=info" />
+            </div>
+            <div className="storage__list-details">
+              <h3>Fiat Mobi Like 1.0</h3>
+              <p>Ano mod: 2011</p>
+              <p>Ano Fab: 2010</p>
+              <p>Cor: Vermelho</p>
+              <p className="storage__list-details-preco">R$ 50.000</p>
+            </div>
+          </li>
+          <li>
+            <div className="storage__list-image">
+              <img src="https://quatrorodas.abril.com.br/wp-content/uploads/2024/02/FiatMobiLikeMY245.jpg?quality=70&strip=info" />
+            </div>
+            <div className="storage__list-details">
+              <h3>Fiat Mobi Like 1.0</h3>
+              <p>Ano mod: 2011</p>
+              <p>Ano Fab: 2010</p>
+              <p>Cor: Vermelho</p>
+              <p className="storage__list-details-preco">R$ 50.000</p>
+            </div>
+          </li>
+          <li>
+            <div className="storage__list-image">
+              <img src="https://quatrorodas.abril.com.br/wp-content/uploads/2024/02/FiatMobiLikeMY245.jpg?quality=70&strip=info" />
+            </div>
+            <div className="storage__list-details">
+              <h3>Fiat Mobi Like 1.0</h3>
+              <p>Ano mod: 2011</p>
+              <p>Ano Fab: 2010</p>
+              <p>Cor: Vermelho</p>
+              <p className="storage__list-details-preco">R$ 50.000</p>
+            </div>
+          </li>
+          <li>
+            <div className="storage__list-image">
+              <img src="https://quatrorodas.abril.com.br/wp-content/uploads/2024/02/FiatMobiLikeMY245.jpg?quality=70&strip=info" />
+            </div>
+            <div className="storage__list-details">
+              <h3>Fiat Mobi Like 1.0</h3>
+              <p>Ano mod: 2011</p>
+              <p>Ano Fab: 2010</p>
+              <p>Cor: Vermelho</p>
+              <p className="storage__list-details-preco">R$ 50.000</p>
+            </div>
+          </li>
+          <li>
+            <div className="storage__list-image">
+              <img src="https://quatrorodas.abril.com.br/wp-content/uploads/2024/02/FiatMobiLikeMY245.jpg?quality=70&strip=info" />
+            </div>
+            <div className="storage__list-details">
+              <h3>Fiat Mobi Like 1.0</h3>
+              <p>Ano mod: 2011</p>
+              <p>Ano Fab: 2010</p>
+              <p>Cor: Vermelho</p>
+              <p className="storage__list-details-preco">R$ 50.000</p>
+            </div>
+          </li>
+          <li>
+            <div className="storage__list-image">
+              <img src="https://quatrorodas.abril.com.br/wp-content/uploads/2024/02/FiatMobiLikeMY245.jpg?quality=70&strip=info" />
+            </div>
+            <div className="storage__list-details">
+              <h3>Fiat Mobi Like 1.0</h3>
+              <p>Ano mod: 2011</p>
+              <p>Ano Fab: 2010</p>
+              <p>Cor: Vermelho</p>
+              <p className="storage__list-details-preco">R$ 50.000</p>
+            </div>
+          </li>
+          <li>
+            <div className="storage__list-image">
+              <img src="https://quatrorodas.abril.com.br/wp-content/uploads/2024/02/FiatMobiLikeMY245.jpg?quality=70&strip=info" />
+            </div>
+            <div className="storage__list-details">
+              <h3>Fiat Mobi Like 1.0</h3>
+              <p>Ano mod: 2011</p>
+              <p>Ano Fab: 2010</p>
+              <p>Cor: Vermelho</p>
+              <p className="storage__list-details-preco">R$ 50.000</p>
+            </div>
+          </li>
+          
         </ul>
       </div>
-      <div className="storage__list">
-        <table>
-          <thead>
-            <tr>
-              <th>Nome</th>
-              <th>Preço</th>
-              <th>Cor</th>
-              <th>Km</th>
-              <th>Ano Fabricação</th>
-              <th>Ano Modelo</th>
-            </tr>
-          </thead>
-          <tbody>
-            {storage.map((carro, index) => (
-              <tr key={index}>
-                <td>{carro.nome}</td>
-                <td>{carro.preco}</td>
-                <td>{carro.cor}</td>
-                <td>{carro.km}</td>
-                <td>{carro.anoFabricacao}</td>
-                <td>{carro.anoModelo}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      {addEditCar()}
     </div>
   );
 }
-
-Storage.propTypes = {
-  storage: PropTypes.arrayOf(
-    PropTypes.shape({
-      nome: PropTypes.string.isRequired,
-      preco: PropTypes.number.isRequired,
-      cor: PropTypes.string.isRequired,
-      km: PropTypes.number.isRequired,
-      anoFabricacao: PropTypes.number.isRequired,
-      anoModelo: PropTypes.number.isRequired,
-    })
-  ).isRequired,
-};
-
-Storage.defaultProps = {
-  storage: [],
-};
